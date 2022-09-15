65fe4f0aa2cc04aec755d07068f03c9732f64271
Marcelo Sebastián Aguirre Alfaro <marcelo.sebastian97@gmail.com>
2022-09-15 16:48:02 -0500

    deploy

--- personal/src/index.js
+++ personal/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Game from './components/Game/Game'
+import Menu from './components/Menu/Menu'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
